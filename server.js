f6837ee4549535e088b65496da5b723151513660
Your Name <you@example.com>
2024-01-26 20:44:44 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}!`));
-
+// Define your route before starting the server
 app.get('/', (req, res) => {
     res.send({ message: "'Hello World!' from server.js" });
-})
+});
+
+// Start the server
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}!`));
